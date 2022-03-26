5224b379207a83a64c210934d6fb0cf4266f749b
Honoluluchampi <msd13aj@gmail.com>
2022-03-26 21:32:51 +0900

    index buffer, staging buffer

--- include/hve_model.hpp
+++ include/hve_model.hpp
@@ -17,11 +17,19 @@ class HveModel
     {
       glm::vec3 position_m;
       glm::vec3 color_m;
+      // return a description compatible with the shader
       static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
       static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
     };
 
-    HveModel(HveDevice& device, const std::vector<Vertex> &vertices);
+    struct Builder
+    {
+      // copied to the vertex buffer and index buffer
+      std::vector<Vertex> vertices_m{};
+      std::vector<uint32_t> indices_m{};
+    };
+
+    HveModel(HveDevice& device, const HveModel::Builder &builder);
     ~HveModel();
 
     HveModel(const HveModel &) = delete;
@@ -32,10 +40,17 @@ class HveModel
 
   private:
     void createVertexBuffers(const std::vector<Vertex> &vertices);
+    void createIndexBuffers(const std::vector<uint32_t> &indices);
+
     HveDevice& hveDevice_m;
     // separating buffer and its assigned memory enables programmer to manage memory manually
     VkBuffer vertexBuffer_m;
     VkDeviceMemory vertexBufferMemory_m;
     uint32_t vertexCount_m;
+
+    bool hasIndexBuffer_m = false;
+    VkBuffer indexBuffer_m;
+    VkDeviceMemory indexBufferMemory_m;
+    uint32_t indexCount_m;
 };
 } // namespace hve
--- src/hve_model.cpp
+++ src/hve_model.cpp
@@ -5,15 +5,21 @@
 
 namespace hve {
 
-HveModel::HveModel(HveDevice& device, const std::vector<Vertex> &vertices) : hveDevice_m{device}
+HveModel::HveModel(HveDevice& device, const HveModel::Builder &builder) : hveDevice_m{device}
 {
-  createVertexBuffers(vertices);
+  createVertexBuffers(builder.vertices_m);
+  createIndexBuffers(builder.indices_m);
 }
 
 HveModel::~HveModel()
 {
   vkDestroyBuffer(hveDevice_m.device(), vertexBuffer_m, nullptr);
   vkFreeMemory(hveDevice_m.device(), vertexBufferMemory_m, nullptr);
+
+  if (hasIndexBuffer_m) {
+    vkDestroyBuffer(hveDevice_m.device(), indexBuffer_m, nullptr);
+    vkFreeMemory(hveDevice_m.device(), indexBufferMemory_m, nullptr);
+  }
 }
 
 void HveModel::createVertexBuffers(const std::vector<Vertex> &vertices)
@@ -22,23 +28,89 @@ void HveModel::createVertexBuffers(const std::vector<Vertex> &vertices)
   // vertexCount must be larger than 3 (triangle) 
   // use a host visible buffer as temporary buffer, use a device local buffer as actual vertex buffer
   VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount_m;
+
+  // copy the data to the staging buffer
+  VkBuffer stagingBuffer;
+  VkDeviceMemory stagingBufferMemory;
   hveDevice_m.createBuffer(
     bufferSize,
-    VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+    VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
     VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+    stagingBuffer,
+    stagingBufferMemory
+  );
+
+  // filling in the data to the staging buffer
+  void *data;
+  vkMapMemory(hveDevice_m.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+  std::memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+  vkUnmapMemory(hveDevice_m.device(), stagingBufferMemory);
+
+  hveDevice_m.createBuffer(
+    bufferSize,
+    VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, // optimal type of the memory type
     vertexBuffer_m,
     vertexBufferMemory_m
   );
-  // filling in the data
+
+  // copy the data from staging buffer to the vertex buffer
+  hveDevice_m.copyBuffer(stagingBuffer, vertexBuffer_m, bufferSize);
+  // clean up the staging buffer no longer necessary
+  vkDestroyBuffer(hveDevice_m.device(), stagingBuffer, nullptr);
+  vkFreeMemory(hveDevice_m.device(), stagingBufferMemory, nullptr);
+}
+
+void HveModel::createIndexBuffers(const std::vector<uint32_t> &indices)
+{
+  indexCount_m = static_cast<uint32_t>(indices.size());
+  // if there is no index, nothing to do
+  hasIndexBuffer_m = indexCount_m > 0;
+  if (!hasIndexBuffer_m) return;
+
+  // vertexCount must be larger than 3 (triangle) 
+  // use a host visible buffer as temporary buffer, use a device local buffer as actual vertex buffer
+  VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount_m;
+
+  // copy the data to the staging buffer
+  VkBuffer stagingBuffer;
+  VkDeviceMemory stagingBufferMemory;
+  hveDevice_m.createBuffer(
+    bufferSize,
+    VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+    VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+    stagingBuffer,
+    stagingBufferMemory
+  );
+
+  // filling in the data to the staging buffer
   void *data;
-  vkMapMemory(hveDevice_m.device(), vertexBufferMemory_m, 0, bufferSize, 0, &data);
-  std::memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-  vkUnmapMemory(hveDevice_m.device(), vertexBufferMemory_m);
+  vkMapMemory(hveDevice_m.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+  std::memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+  vkUnmapMemory(hveDevice_m.device(), stagingBufferMemory);
+
+  hveDevice_m.createBuffer(
+    bufferSize,
+    VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, // optimal type of the memory type
+    indexBuffer_m,
+    indexBufferMemory_m
+  );
+
+  // copy the data from staging buffer to the vertex buffer
+  hveDevice_m.copyBuffer(stagingBuffer, indexBuffer_m, bufferSize);
+
+  // clean up the staging buffer no longer necessary
+  vkDestroyBuffer(hveDevice_m.device(), stagingBuffer, nullptr);
+  vkFreeMemory(hveDevice_m.device(), stagingBufferMemory, nullptr);
 }
 
 void HveModel::draw(VkCommandBuffer commandBuffer)
 {
-  vkCmdDraw(commandBuffer, vertexCount_m, 1, 0, 0);
+  if (hasIndexBuffer_m)
+    vkCmdDrawIndexed(commandBuffer, indexCount_m, 1, 0, 0, 0);
+  else 
+    vkCmdDraw(commandBuffer, vertexCount_m, 1, 0, 0);
 }
 
 void HveModel::bind(VkCommandBuffer commandBuffer)
@@ -46,6 +118,10 @@ void HveModel::bind(VkCommandBuffer commandBuffer)
   VkBuffer buffers[] = {vertexBuffer_m};
   VkDeviceSize offsets[] = {0};
   vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+
+  // last parameter should be same as the type of the Build::indices
+  if (hasIndexBuffer_m) 
+    vkCmdBindIndexBuffer(commandBuffer, indexBuffer_m, 0, VK_INDEX_TYPE_UINT32);
 }
 
 std::vector<VkVertexInputBindingDescription> HveModel::Vertex::getBindingDescriptions()
