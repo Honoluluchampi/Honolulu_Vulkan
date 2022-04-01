42fdccd3ecba6ecca472a02cb810eae823e0ab15
Honoluluchampi <msd13aj@gmail.com>
2022-04-01 19:35:01 +0900

    structure changed

---
+++ setup-env.sh
@@ -0,0 +1,5 @@
+HNLL_ENGN=~/programs/honolulu_engines
+# sh ~/programs/external_libraries/vulkanSDK/setup-env.sh
+export HVE_DIR=$HNLL_ENGN/honolulu_vulkan_engine
+export HGE_DIR=$HNLL_ENGN/honolulu_game_engine
+export MODEL_DIR=$HNLL_ENGN/models
