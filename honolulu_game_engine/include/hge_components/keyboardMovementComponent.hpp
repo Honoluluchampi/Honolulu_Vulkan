a370e1280b05b25882e01797f7a5fa61e4394cbc
Honoluluchampi <msd13aj@gmail.com>
2022-05-13 20:21:46 +0900

    debug gc

--- honolulu_game_engine/include/hge_components/keyboardMovementComponent.hpp
+++ honolulu_game_engine/include/hge_components/keyboardMovementComponent.hpp
@@ -11,36 +11,73 @@ namespace hnll {
 class KeyboardMovementComponent : public HgeComponent
 {
   public:
-    KeyboardMovementComponent(GLFWwindow* window, Transform& transform) 
-      : HgeComponent(), window_m(window), transform_m(transform){}
+    using KeyId = int;
+    using AxisId = int;
+    using ButtonMap = std::unordered_map<KeyId, u_ptr<std::function<void()>>>;
+    using AxisMap = std::unordered_map<AxisId, u_ptr<std::function<glm::vec3(float,float)>>>;
+    
+    KeyboardMovementComponent(GLFWwindow* window, Transform& transform);
     
     struct KeyMappings 
     {
-      int moveLeft = GLFW_KEY_A;
-      int moveRight = GLFW_KEY_D;
-      int moveForward = GLFW_KEY_W;
-      int moveBackward = GLFW_KEY_S;
-      int moveUp = GLFW_KEY_E;
-      int moveDown = GLFW_KEY_Q;
-      int lookLeft = GLFW_KEY_LEFT;
-      int lookRight = GLFW_KEY_RIGHT;
-      int lookUp = GLFW_KEY_UP;
-      int lookDown = GLFW_KEY_DOWN;
+      KeyId moveLeft = GLFW_KEY_A;
+      KeyId moveRight = GLFW_KEY_D;
+      KeyId moveForward = GLFW_KEY_W;
+      KeyId moveBackward = GLFW_KEY_S;
+      KeyId moveUp = GLFW_KEY_E;
+      KeyId moveDown = GLFW_KEY_Q;
+      KeyId lookLeft = GLFW_KEY_LEFT;
+      KeyId lookRight = GLFW_KEY_RIGHT;
+      KeyId lookUp = GLFW_KEY_UP;
+      KeyId lookDown = GLFW_KEY_DOWN;
+    };
+
+    struct AxisMappings
+    {
+      AxisId moveX = GLFW_GAMEPAD_AXIS_LEFT_X;
+      AxisId moveY = GLFW_GAMEPAD_AXIS_LEFT_X;
+      AxisId rotaX = GLFW_GAMEPAD_AXIS_RIGHT_Y;
+      AxisId rotaY = GLFW_GAMEPAD_AXIS_RIGHT_X;
     };
 
     // update owner's position (owner's transformation's ref was passed in the ctor)
-    inline void updateComponent(float dt) override
-    { moveInPlaneXZ(dt); }
+    void updateComponent(float dt) override;
 
     void moveInPlaneXZ(float dt);
 
+    // dont use lambda's capture
+    // TODO : check whether using checkingButtonList fasten the checking sequence
+    void setButtonFunc(KeyId keyId, std::function<void()> func)
+    { buttonMap_.emplace(keyId, std::make_unique<std::function<void()>>(func)); }
+    // dont use lambda's capture
+    void setAxisFunc(AxisId axisId, std::function<glm::vec3(float, float)> func)
+    { axisMap_.emplace(axisId, std::make_unique<std::function<glm::vec3(float, float)>>(func)); }
+
+    void removeButtonFunc(KeyId keyId)
+    { buttonMap_.erase(keyId); }
+    void removeAxisFunc(AxisId axisId)
+    { axisMap_.erase(axisId); }
+
   private:
-    KeyMappings keys{};
+    void setDefaultMapping();
+    void processRotateInput(GLFWgamepadstate& state, float dt);
+    void processMoveInput(GLFWgamepadstate& state, float dt){}
+    void processButtonInput(GLFWgamepadstate& state, float dt){}
+
+    // mapping
+    static KeyMappings keys;
+    static AxisMappings axis;
+    
     GLFWwindow* window_m;
-    // this component should be delete before the owner
+    // this component should be deleted before the owner
     Transform& transform_m;
     float moveSpeed{3.f};
     float lookSpeed{1.5f};
+
+    ButtonMap buttonMap_;
+    // funcs take GLFWgamepadstate.axes[glfw_gamepad_axis_leftorright_xory], dt
+    AxisMap axisMap_;
+    int padId = GLFW_JOYSTICK_1;
 };
 
 } // namespace hnll
--- honolulu_game_engine/src/hge_component/keyboardMovementComponent.cpp
+++ honolulu_game_engine/src/hge_component/keyboardMovementComponent.cpp
@@ -5,25 +5,57 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 
+constexpr float MOVE_THRESH = 0.1f; 
+constexpr float ROTATE_THRESH = 0.1f;
+
 namespace hnll {
 
-void KeyboardMovementComponent::moveInPlaneXZ(float dt)
+KeyboardMovementComponent::KeyMappings KeyboardMovementComponent::keys{};
+KeyboardMovementComponent::AxisMappings KeyboardMovementComponent::axis{};
+
+KeyboardMovementComponent::KeyboardMovementComponent(GLFWwindow* window, Transform& transform)
+  : HgeComponent(), window_m(window), transform_m(transform)
+{
+    // mapping
+  const char* mapping = "03000000790000004618000010010000,Nintendo GameCube Controller Adapter,a:b1,b:b2,dpdown:b14,dpleft:b15,dpright:b13,dpup:b12,lefttrigger:b4,leftx:a0,lefty:a1,leftshoulder:b4,rightshoulder:b5,guide:b7,rightx:a5~,righty:a2~,start:b9,x:b0,y:b3,platform:Linux";
+  glfwUpdateGamepadMappings(mapping);
+}
+
+void KeyboardMovementComponent::updateComponent(float dt)
+{
+  GLFWgamepadstate state;
+  glfwGetGamepadState(padId, &state);
+  processRotateInput(state, dt);
+  // temp
+  moveInPlaneXZ(dt);
+  processMoveInput(state,dt);
+  processButtonInput(state, dt);
+}
+
+void KeyboardMovementComponent::processRotateInput(GLFWgamepadstate& state, float dt)
 {
   // store user input
-  glm::vec3 rotate{0};
-  if (glfwGetKey(window_m, keys.lookRight) == GLFW_PRESS) rotate.y += 1.f;
-  if (glfwGetKey(window_m, keys.lookLeft) == GLFW_PRESS) rotate.y -= 1.f;
-  if (glfwGetKey(window_m, keys.lookUp) == GLFW_PRESS) rotate.x += 1.f;
-  if (glfwGetKey(window_m, keys.lookDown) == GLFW_PRESS) rotate.x -= 1.f;
-
-  // if rotate vec is zero, dont change the rotateion matrix of gameObject
+  float moveX = state.axes[axis.moveX];
+  float moveY = state.axes[axis.moveY];
+  
+  float rotaX = state.axes[axis.rotaX] 
+              + (glfwGetKey(window_m, keys.lookUp) == GLFW_PRESS)
+              - (glfwGetKey(window_m, keys.lookDown) == GLFW_PRESS);
+  float rotaY = state.axes[axis.rotaY]
+              + (glfwGetKey(window_m, keys.lookRight) == GLFW_PRESS)
+              - (glfwGetKey(window_m, keys.lookLeft) == GLFW_PRESS);
+  glm::vec3 rotate = {rotaX, rotaY, 0.f};
+  // add to the current rotate matrix
   if (glm::dot(rotate, rotate) > std::numeric_limits<float>::epsilon())
     transform_m.rotation_m += lookSpeed * dt * glm::normalize(rotate);
 
   // limit pitch values between about +/- 58ish degrees
   transform_m.rotation_m.x = glm::clamp(transform_m.rotation_m.x, -1.5f, 1.5f);
   transform_m.rotation_m.y = glm::mod(transform_m.rotation_m.y, glm::two_pi<float>());
+}
 
+void KeyboardMovementComponent::moveInPlaneXZ(float dt)
+{
   float yaw = transform_m.rotation_m.y;
   const glm::vec3 forwardDir{sin(yaw), 0.f, cos(yaw)};
   const glm::vec3 rightDir{forwardDir.z, 0.f, -forwardDir.x};
@@ -41,4 +73,11 @@ void KeyboardMovementComponent::moveInPlaneXZ(float dt)
   if (glm::dot(moveDir, moveDir) > std::numeric_limits<float>::epsilon())
     transform_m.translation_m += moveSpeed * dt * glm::normalize(moveDir);
 }
+
+void KeyboardMovementComponent::setDefaultMapping()
+{
+  // axis
+  auto leftXfunc = [](float val){return glm::vec3(0.f, 0.f, 0.f); };
+}
+
 } // namespace hnll
