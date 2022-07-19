68808a1ca099050aeaf874a29f4837acd8885583
Honoluluchampi <msd13aj@gmail.com>
2022-07-20 02:19:48 +0900

    mass and dxdt lambda

--- include/physics/particle_component.hpp
+++ include/physics/particle_component.hpp
@@ -2,6 +2,10 @@
 
 // hnll
 #include <utils/utils.hpp>
+#include <game/component.hpp>
+
+//std
+#include <functional>
 
 namespace hnll {
 
@@ -10,7 +14,7 @@ namespace game { class actor; }
 
 namespace physics {
 
-class particle_component
+class particle_component :public hnll::game::component
 {
   public:
     static s_ptr<particle_component> create(game::actor& actor);
@@ -21,12 +25,25 @@ class particle_component
     particle_component(const particle_component&) = delete;
     particle_component& operator=(const particle_component&) = delete;
 
+    // getter
+    bool is_gravity_enabled() const { return is_gravity_enabled_; }
+
+    // setter
+    bool set_gravity_state(bool state, double value = 9.8f)
+    { if (state) gravity_value_ = value; is_gravity_enabled_ = state; }
+
   private:
+    // solve ode
+    void update_component(float dt) override;
     // TODO : consider to delete s_ptr<transform>
     s_ptr<hnll::utils::transform> transform_sp_;
-    glm::vec3& position_ref_;
+    glm::vec3& position_;
     glm::vec3 velocity_;
+    double mass_;
+    std::function<void(float dt)> dx_dt_;
     float radius_ = 1.f;
+    bool is_gravity_enabled_ = true;
+    double gravity_value_ = 9.8f;
 };
 
 } // namespace physics
--- modules/physics/src/particle_component.cpp
+++ modules/physics/src/particle_component.cpp
@@ -2,19 +2,34 @@
 #include <physics/particle_component.hpp>
 #include <game/actor.hpp>
 
+// lib
+#include <eigen3/Eigen/Dense>
+
 using hnll::utils::transform;
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
 
 namespace hnll {
 namespace physics {
 
 particle_component::particle_component(s_ptr<hnll::utils::transform>&& transform_sp)
- : transform_sp_(std::move(transform_sp)), position_ref_(transform_sp->translation)
-{}
+ : transform_sp_(std::move(transform_sp)), position_(transform_sp->translation)
+{
+  dx_dt_ = [this](float dt) {
+    this->position_ += velocity_ * dt;
+    this->velocity_ += gravity_value_ * dt; // m * g * dt / m
+  };
+}
 
 s_ptr<particle_component> particle_component::create(game::actor &actor)
 {
   auto particle = std::make_shared<particle_component>(actor.get_transform_sp());
   return particle;
+}
+
+void particle_component::update_component(float dt)
+{
+  dx_dt_(dt);
 }
 
 } // namespace physics
