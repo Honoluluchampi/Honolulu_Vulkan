d9902fd36aebf3e9ab92207eff5a93d74fca01bd
Honoluluchampi <msd13aj@gmail.com>
2022-09-08 19:09:43 +0900

    add falling ball app

--- applications/falling_ball/src/falling_ball.cpp
+++ applications/falling_ball/src/falling_ball.cpp
@@ -1,4 +1,13 @@
+#include <game/engine.hpp>
+
 int main()
 {
+  hnll::game::engine app{"falling ball"};
+  try { app.run(); }
+  catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
 
+  return EXIT_SUCCESS;
 }
