0929bcbdcc9a7040f9e5030e0c6d7772f738c406
Honoluluchampi <msd13aj@gmail.com>
2022-09-22 23:56:23 +0900

    add hnll::game::component::specific_add_process

--- include/physics/rigid_component.hpp
+++ include/physics/rigid_component.hpp
@@ -1,20 +1,25 @@
 #pragma once
 
 // hnll
+#include <game/component.hpp>
 #include <utils/utils.hpp>
 #include <physics/bounding_volume.hpp>
 
 namespace hnll::physics {
 
-class rigid_component
+class rigid_component : public hnll::game::component
 {
   public:
     rigid_component(bounding_volume& bv) : bounding_volume_(bv){}
 
+    void specific_add_process(hnll::game::actor& owner) override;
     // getter
-    const bounding_volume&  get_bounding_volume() const { return bounding_volume_; }
-    const utils::transform& get_transform() const { return *transform_sp_; }
+    const bounding_volume&        get_bounding_volume() const { return bounding_volume_; }
+    const utils::transform&       get_transform() const { return *transform_sp_; }
+    const s_ptr<utils::transform> get_transform_sp() const { return transform_sp_; }
 
+    // setter
+    void set_transform(s_ptr<utils::transform>& transform_sp) { transform_sp_ = transform_sp; }
   private:
     s_ptr<hnll::utils::transform> transform_sp_;
     bounding_volume bounding_volume_;
--- modules/physics/src/rigid_component.cpp
+++ modules/physics/src/rigid_component.cpp
@@ -1,5 +1,12 @@
+// hnll
+#include <game/actor.hpp>
 #include <physics/rigid_component.hpp>
 
 namespace hnll::physics {
+
+void hnll::physics::rigid_component::specific_add_process(hnll::game::actor &owner)
+{
+  this->transform_sp_ = owner.get_transform_sp();
+}
 
 } // hnll::physics
