5291abac9866a1d1a258b1f4c9c3ba56bcbf2f18
Honoluluchampi <msd13aj@gmail.com>
2022-11-22 00:46:01 +0900

    add stable dx, dt

--- include/physics/fdtd_solver.hpp
+++ include/physics/fdtd_solver.hpp
@@ -10,10 +10,23 @@ template<typename T> using s_ptr = std::shared_ptr<T>;
 
 namespace physics {
 
+// temporally one-dimensional implementation
 class fdtd_solver
 {
   public:
     fdtd_solver() = default;
+
+    // _duration is in seconds
+    void solve(std::vector<double> _input, double _duration);
+
+    // setter
+    fdtd_solver* set_dx(double _dx) { dx_ = _dx; return this; }
+    fdtd_solver* set_dt(double _dt) { dt_ = _dt; return this; }
+    fdtd_solver* set_stiffness(double _sn)      { stiffness_ = _sn;      return this; }
+    fdtd_solver* set_density_(double _ds)       { density_ = _ds;        return this; }
+    fdtd_solver* set_phase_velocity(double _pv) { phase_velocity_ = _pv; return this; }
+    fdtd_solver* set_max_stable_dx();
+    fdtd_solver* set_min_stable_dt();
 
   private:
     // stable restriction dx_ / dt_ > sqrt(stiffness_ / density_)
@@ -21,6 +34,7 @@ class fdtd_solver
     double dt_;
     double stiffness_;
     double density_;
+    double phase_velocity_ = -1.f;
     std::vector<double> input_;
     std::vector<double> stress_;
     std::vector<double> particle_velocity_;
