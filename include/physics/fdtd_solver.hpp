5f70222e370a339e92a398bac0f7c100cc91dff2
Honoluluchampi <msd13aj@gmail.com>
2022-11-24 14:51:44 +0900

    create input

--- include/physics/fdtd_solver.hpp
+++ include/physics/fdtd_solver.hpp
@@ -14,14 +14,23 @@ namespace physics {
 class fdtd_solver
 {
   public:
+    static u_ptr<fdtd_solver> create() { return std::make_unique<fdtd_solver>(); }
+
     fdtd_solver() = default;
 
     // _duration is in seconds
     void solve(std::vector<double> _input, double _duration);
 
     // setter
-    fdtd_solver* set_dx(double _dx) { dx_ = _dx; return this; }
-    fdtd_solver* set_dt(double _dt) { dt_ = _dt; return this; }
+    double get_dx()             const { return dx_; }
+    double get_dt()             const { return dt_; }
+    double get_stiffness()      const { return stiffness_; }
+    double get_density_()       const { return density_; }
+    double get_phase_velocity() const { return phase_velocity_; }
+
+    // setter
+    fdtd_solver* set_dx(double _dx)             { dx_ = _dx; return this; }
+    fdtd_solver* set_dt(double _dt)             { dt_ = _dt; return this; }
     fdtd_solver* set_stiffness(double _sn)      { stiffness_ = _sn;      return this; }
     fdtd_solver* set_density_(double _ds)       { density_ = _ds;        return this; }
     fdtd_solver* set_phase_velocity(double _pv) { phase_velocity_ = _pv; return this; }
@@ -35,7 +44,6 @@ class fdtd_solver
     double stiffness_;
     double density_;
     double phase_velocity_ = -1.f;
-    std::vector<double> input_;
     std::vector<double> stress_;
     std::vector<double> particle_velocity_;
 };
