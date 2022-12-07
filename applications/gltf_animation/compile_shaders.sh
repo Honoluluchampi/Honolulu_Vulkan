d669f25a988010413c7036459b9865349423b207
Honoluluchampi <msd13aj@gmail.com>
2022-12-07 19:54:07 +0900

    add vertex shader

---
+++ applications/gltf_animation/compile_shaders.sh
@@ -0,0 +1,17 @@
+COMPILER=${VULKAN_DIR}/bin/glslangValidator
+TARGET_ENV=vulkan1.3
+
+SHADERDIR=shaders
+OUTPUTDIR=shaders/spv
+mkdir -p ${OUTPUTDIR}
+
+extensions=("vert" "frag")
+
+for extension in ${extensions[@]}
+  do
+    for FILE in ${SHADERDIR}/*.${extension}.glsl
+      do
+        FILENAME=$(basename ${FILE})
+        ${COMPILER} -S ${extension} ${FILE} --target-env ${TARGET_ENV} -o ${OUTPUTDIR}/${FILENAME}.spv
+    done
+done
